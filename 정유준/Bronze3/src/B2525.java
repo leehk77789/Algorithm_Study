dc9e8c980183b7529e749d1ef0710299c804ff55
leehk77789 <leehk77789@gmail.com>
2023-02-01 16:38:14 +0900

    [완료] 23/02/01 2525

---
+++ 정유준/Bronze3/src/B2525.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class B2525 {
+
+	public static void main(String[] args) {
+		//14 30
+		//20
+		//17 40
+		//80
+		//23 48
+		//25
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int time = B+C;
+		
+		if(A + (time/60) >= 24) {
+			A += (time / 60);
+			A -= 24;
+			time %= 60;
+		} else {
+			A += (time / 60);
+			time %= 60; 
+		}
+		System.out.println(A + " " + time);
+		
+	}
+}
