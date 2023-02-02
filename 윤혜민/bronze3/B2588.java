034b34e5e04fed0b2697b4bd7ccde34d530e44e8
hyeii <121pleiades@gmail.com>
2023-02-02 22:39:48 +0900

    [완료] 230202_B2588

--- 윤혜민/bronze3/B2588.java
+++ 윤혜민/bronze3/B2588.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package algorithm;
 
 import java.util.*;
 
@@ -7,40 +7,26 @@ public class B2588 {
 
 		Scanner sc = new Scanner(System.in);
 
-		int H = sc.nextInt();
-		int M = sc.nextInt();
-		int oven = sc.nextInt();
+		// 두 정수를 입력받는다
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 
-		int hour = 0;
-		int min = 0;
+		// 출력값이 들어갈 배열을 생성한다
+		int[] numList = new int[3];
 
-		hour = H + oven / 60;
-		if (hour >= 24) {
-			hour %= 24;
+		// 두번째 정수의 자릿수를 계산한다
+		// 첫번째 출력값의 경우 두번쨰 정수를 10으로 나누었을 때 나머지를 첫번째 정수와 곱한 값이다.
+		for (int i = 0; i < 3; i++) {
+			numList[i] = num1 * (num2 % 10);
+			num2 /= 10;
 		}
 
-		min = M + oven % 60;
-		if (min >= 60) {
-			min %= 60;
-			hour++;
-		}
-//		if (H + oven / 60 < 24) {
-//			hour = (H + oven / 60);
-//
-//		} else if (H + oven / 60 >= 24) {
-//			hour = (H + oven / 60) - 24;
-//		}
-//
-//		if (M + oven % 60 < 60) {
-//			min = M + oven % 60;
-//		} else if (M + oven % 60 >= 60) {
-//			min = M + oven % 60 - 60;
-//			hour += 1;
-//		}
-		if (hour == 24)
-			hour = 0;
+		// 배열에 들어간 수들을 곱셈으로 만들어준다.
+		int multiple = numList[0] + numList[1] * 10 + numList[2] * 100;
 
-		System.out.println(hour + " " + min);
+		System.out.println(numList[0]);
+		System.out.println(numList[1]);
+		System.out.println(numList[2]);
+		System.out.println(multiple);
 	}
-
 }
