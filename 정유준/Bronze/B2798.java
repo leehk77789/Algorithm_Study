98a6fce267ce8baa443696d7790376cad8490032
namjin <skawls25@gmail.com>
2023-02-14 17:44:21 +0900

    [완료] 23/02/14 2798

--- 정유준/Bronze/B2798.java
+++ 정유준/Bronze/B2798.java
@@ -9,24 +9,25 @@ public class B2798 {
 		int dealerNum = sc.nextInt();
 		int[] cardArr= new int[cardNum];
 		
+		int sum = 0;
+		int result = 0;
 		//카드값 입력받기
 		for(int idx = 0; idx < cardNum; idx++) {
 			cardArr[idx] = sc.nextInt();
 		}
 		
-		int min = 300000;
 		//for문 3개돌리면서 최소차이 찾기 ^^
 		for(int i = 0; i < cardNum; i++) {
 			for(int j = i+1; j < cardNum; j++) {
 				for(int z = j+1; z < cardNum; z++) {
-					if(dealerNum - cardArr[i] - cardArr[j] - cardArr[z] < min && dealerNum - cardArr[i] - cardArr[j] - cardArr[z] >= 0) {
-						min = cardArr[i] + cardArr[j] + cardArr[z];
+					sum = cardArr[i] + cardArr[j] + cardArr[z];
+					if(sum <= dealerNum && result < sum) {
+						result = sum;
 					}
 				}
 			}
 		}
-		System.out.println(min);
 		
-		
+		System.out.println(result);
 	}
 }
