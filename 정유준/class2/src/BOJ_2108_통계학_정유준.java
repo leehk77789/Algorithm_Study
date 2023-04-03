2d4f3eeda561d20c86f5534bfddd27969842ed35
yoojun <leehk77789@gmail.com>
2023-04-03 21:02:37 +0900

    0403

--- 정유준/class2/src/BOJ_2108_통계학_정유준.java
+++ 정유준/class2/src/BOJ_2108_통계학_정유준.java
@@ -4,70 +4,51 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class BOJ_2108_통계학_정유준 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static int[] num;
-	static int[][] count;
-	static int[] compare;
 
-	static void input() throws NumberFormatException, IOException {
-		int numAmount = Integer.parseInt(br.readLine());
-		num = new int[numAmount];
-		for (int i = 0; i < numAmount; i++) {
-			num[i] = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+		int[] cnt = new int[8001];
+
+		int sum = 0; // 평균 구하기 위해
+		int mode = 0; // 최빈값
+
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			arr[i] = tmp;
+			cnt[tmp + 4000]++;
+			sum += tmp;
 		}
-		count = new int[num.length][1];
-		compare = new int[num.length];
-		Arrays.sort(num);
-	}
 
-	static void addSum() {
-		int sum = 0;
-		for (int i = 0; i < num.length; i++) {
-			sum += num[i];
-		}
-		System.out.println(sum / num.length);
-	}
+		Arrays.sort(arr);
 
-	static void sort() {
-		sb.append(num[num.length / 2]).append("\n");
-	}
+		// '두 번째' 작은 값인지 확인하기 위해
+		boolean flag = false;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < 8001; i++) {
+			if (cnt[i] == 0)
+				continue;
 
-	static void mostSeen() {
-		int max = 0;
-		for (int i = 0; i < num.length; i++) {
-			count[i][0]++;
-		}
-		for (int i = 0; i < count.length; i++) {
-			if (count[i][0] > max) {
-				max = count[i][0];
+			// 첫번째 작은 값
+			if (cnt[i] > max) {
+				max = cnt[i];
+				mode = i - 4000;
+				flag = true;
+			}
+			// 두번째 작은 값
+			else if (flag && max == cnt[i]) {
+				mode = i - 4000;
+				flag = false;
 			}
 		}
-		int same = 0;
-		for (int i = 0; i < count.length; i++) {
-			if (count[i][0] == max) {
-				same++;
-				if (same == 2) {
-					sb.append(num[count[i][0]]).append("\n");
-				}
-			}
-		}
-	}
 
-	static void maxMin() {
-		sb.append(num[num.length - 1] - num[0]);
-	}
-
-	static void solve() throws NumberFormatException, IOException {
-		input();
-		addSum();
-		sort();
-		mostSeen();
-		maxMin();
-		System.out.println(sb);
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		solve();
+		System.out.println((int) Math.round((double) sum / N));
+		System.out.println(arr[N / 2]);
+		System.out.println(mode);
+		System.out.println(arr[N - 1] - arr[0]);
 	}
 }
